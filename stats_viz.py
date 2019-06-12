526688d3cce0295bb108d92ba2834530001ee257
Michael Hemenway <textpotential@users.noreply.github.com>
2019-06-12 07:38:42 -0700

    add a few more viz

--- stats_viz.py
+++ stats_viz.py
@@ -5,14 +5,19 @@ from bokeh.models import ColumnDataSource
 # from bokeh.palettes import Spectral7
 from bokeh.models.tools import HoverTool
 
+# LOAD DATA
 df = pd.read_csv('alma_loans_by_date.csv')  # read data to dataframe
 
+# PREPARE DATA
 df['Loan Date'] = pd.to_datetime(df['Loan Date'])  # set column to datetime format
 df.set_index('Loan Date', inplace=True)  #  set index to 'Loan Date' in order to group by month
 g = df.groupby(pd.Grouper(freq="M"))  # group by month
 new_df = g.sum()  # sum groupby object
 new_df.reset_index(inplace=True)  # reset index 
 
+# Transform data - plot
+
+# Loans by month
 source = ColumnDataSource(new_df)
 # create the 'canvas' for the visualization
 p = figure(plot_height=500, plot_width=950, x_axis_type='datetime')
@@ -29,3 +34,20 @@ p.legend.location = 'top_left'
 
 # display visualization
 show(p)
+
+# Loans by Day
+source = ColumnDataSource(df)
+p = figure(plot_height=500, plot_width=950, x_axis_type='datetime')
+p.line(x='Loan Date', y='Loans (In House + Not In House)', line_width=2, source=source, color=Spectral7[0],
+      legend='Number of Loans')
+
+# add metadata to the visualization
+p.yaxis.axis_label = 'Total Loans'
+p.xaxis.axis_label = 'Day'
+p.title.text = 'Iliff Library Loans by Day'
+p.legend.location = 'top_left'
+
+# display visualization
+show(p)
+
+# 10 most popular LC categories? 
